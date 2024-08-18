5c8d30a86b70fb4b61a4ed92aa8def661f5ba955
Eyumussie <87204461+Eyumussie@users.noreply.github.com>
2024-08-18 23:55:45 +0300

    Update urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -11,7 +11,10 @@ urlpatterns = [
     path('login/', views.CustomLoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', views.CustomLogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),  # This is the line you need
-    path('admin-view/', views.admin_view, name='admin_view'),
-    path('librarian-view/', views.librarian_view, name='librarian_view'),
-    path('member-view/', views.member_view, name='member_view'),
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
+    path('add_book/', views.add_book, name='add_book'),  # Updated path for adding a book
+    path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),  # Updated path for editing a book
+    path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),  # Updated path for deleting a book
 ]
