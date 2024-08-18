0e00a9a4c3e76e3843465bfe6b378000bce2e37e
eyoelmussie <eyumussie@gmail.com>
2024-08-18 23:21:30 +0300

    update

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('login/', views.CustomLoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', views.CustomLogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),  # This is the line you need
+    path('admin-view/', views.admin_view, name='admin_view'),
+    path('librarian-view/', views.librarian_view, name='librarian_view'),
+    path('member-view/', views.member_view, name='member_view'),
 ]
