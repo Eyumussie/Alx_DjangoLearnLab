711589d7674e63f2a18ffe959b6c928d3eba357e
Eyumussie <87204461+Eyumussie@users.noreply.github.com>
2024-09-15 17:16:33 +0300

    Update models.py

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+#creating author model with name field
+class Author(models.Model):
+    name = models.CharField(max_length=255) 
+
+    def __str__(self):
+        return self.name
+
+
+#creating book model with title, publication_year and author field
+class Book(models.Model):
+    title = models.CharField(max_length=255)      
+    publication_year = models.IntegerField() 
+    # A foreign key linking to the Author model, establishing a one-to-many relationship.
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)  
+    def __str__(self):
+        return self.title
