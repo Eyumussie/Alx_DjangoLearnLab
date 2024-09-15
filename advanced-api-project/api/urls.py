fbba8622b56f07f0cc6802178570e63a13a991f8
Eyumussie <87204461+Eyumussie@users.noreply.github.com>
2024-09-15 17:28:00 +0300

    Update urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -7,7 +7,4 @@ urlpatterns = [
     path('books/create/', views.BookCreateView.as_view(), name='book-create'),
     path('books/update/<int:pk>/', views.BookUpdateView.as_view(), name='book-update'),
     path('books/delete/<int:pk>/', views.BookDeleteView.as_view(), name='book-delete'),
-    path('authors/', views.AuthorListView.as_view(), name='author-list'),
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # Include api.urls here
 ]
