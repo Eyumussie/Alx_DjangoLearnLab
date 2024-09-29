f295e641bbdc643e4a53de3b0eb3f3547c1030ea
eyoelmussie <eyumussie@gmail.com>
2024-09-29 21:44:32 +0300

    add

--- social_media_api/social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/social_media_api/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for django_blog project.
+URL configuration for social_media_api project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
